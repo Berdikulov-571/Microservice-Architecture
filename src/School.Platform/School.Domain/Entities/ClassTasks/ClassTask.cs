d29f40b519e3d52565dd9acaa4187124f2d9c3eb
berdikulov-571 <mygithub571@gmail.com>
2023-12-03 21:37:02 +0500

    ClassTask

---
+++ src/School.Platform/School.Domain/Entities/ClassTasks/ClassTask.cs
@@ -0,0 +1,19 @@
+﻿using School.Domain.Entities.Classes;
+using School.Domain.Entities.Task;
+using School.Domain.Entities.Teachers;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School.Domain.Entities.ClassTasks
+{
+    public class ClassTask
+    {
+        public int ClassTaskId { get; set; }
+        [ForeignKey(nameof(Teacher))]
+        public int TeacherId { get; set; }
+        [ForeignKey(nameof(Task))]
+        public int TaskId { get; set; }
+        public Teacher Teacher { get; set; }
+        public Tasks Task { get; set; }
+        public DateTime Day { get; set; } = DateTime.Now;
+    }
+}
