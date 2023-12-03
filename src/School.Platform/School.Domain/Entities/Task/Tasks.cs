0c17b19734f05ecebcb7d8ea564060df6991c71d
berdikulov-571 <mygithub571@gmail.com>
2023-12-03 21:37:18 +0500

    Task

---
+++ src/School.Platform/School.Domain/Entities/Task/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using School.Domain.Entities.ClassTasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School.Domain.Entities.Task
+{
+    public class Tasks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+        public ICollection<ClassTask> ClassTasks { get; set; }
+    }
+}
