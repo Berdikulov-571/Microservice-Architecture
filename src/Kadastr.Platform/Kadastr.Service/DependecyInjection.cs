cccce25998d6fcdd46ba504b387e76f920c5b048
berdikulov-571 <mygithub571@gmail.com>
2023-12-03 21:35:44 +0500

    Kadastr.Service

---
+++ src/Kadastr.Platform/Kadastr.Service/DependecyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Kadastr.Service
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
