fead72e0cc78d308de32d0676e49831da21f516b
berdikulov-571 <mygithub571@gmail.com>
2023-12-04 15:53:13 +0500

    MediaHelpers

---
+++ src/School.Platform/School.Service/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace School.Service.Common.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetImageExtensions();
+
+            if (ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "IMG_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            return null;
+        }
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+            ".jpg", ".jpeg",
+            ".png",
+            ".bmp",
+            ".svg"
+            };
+        }
+    }
+}
