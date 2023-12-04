930a661d8adbe890c597144d1eecd8dfdd736a43
berdikulov-571 <mygithub571@gmail.com>
2023-12-04 15:04:38 +0500

    Global Exceptions

---
+++ src/School.Platform/School.Domain/Exceptions/GlobalExceptions/UserNameAlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace School.Domain.Exceptions.GlobalExceptions
+{
+    public class UserNameAlreadyExistsException : Exception
+    {
+        private HttpStatusCode StatusCode { get; } = HttpStatusCode.Forbidden;
+        private string TitleMessage { get; set; } = default!;
+
+        public UserNameAlreadyExistsException()
+        {
+            TitleMessage = "This UserName Already Exists !";
+        }
+    }
+}
