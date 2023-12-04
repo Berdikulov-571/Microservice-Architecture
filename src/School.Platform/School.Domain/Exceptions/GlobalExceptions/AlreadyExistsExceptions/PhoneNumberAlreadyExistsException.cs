8fc6a9f34b378932d7aa3cb6e700aa847cb18547
berdikulov-571 <mygithub571@gmail.com>
2023-12-04 15:09:32 +0500

    Already Exists

---
+++ src/School.Platform/School.Domain/Exceptions/GlobalExceptions/AlreadyExistsExceptions/PhoneNumberAlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace School.Domain.Exceptions.GlobalExceptions.AlreadyExistsExceptions
+{
+    public class PhoneNumberAlreadyExistsException : Exception
+    {
+        private HttpStatusCode StatusCode { get; } = HttpStatusCode.Forbidden;
+        private string TitleMessage { get; set; } = default!;
+
+        public PhoneNumberAlreadyExistsException()
+        {
+            TitleMessage = "This PhoneNumber Already Exists Exception";
+        }
+    }
+}
