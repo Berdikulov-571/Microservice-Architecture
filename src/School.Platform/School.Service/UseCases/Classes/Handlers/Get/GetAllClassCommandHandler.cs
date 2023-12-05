ddcb9f4b13a179e3bfa83f3deb7a276aaf389a18
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:46:21 +0500

    Classes

---
+++ src/School.Platform/School.Service/UseCases/Classes/Handlers/Get/GetAllClassCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Classes;
+using School.Domain.Exceptions.Classes;
+using School.Service.Abstractions.DataAccess;
+using School.Service.UseCases.Classes.Queries.Get;
+
+namespace School.Service.UseCases.Classes.Handlers.Get
+{
+    public class GetAllClassCommandHandler : IRequestHandler<GetAllClassQuery, IEnumerable<Class>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllClassCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Class>> Handle(GetAllClassQuery request, CancellationToken cancellationToken)
+        {
+            List<Class> result = await _context.Classes.ToListAsync(cancellationToken);
+
+            if (result == null)
+                throw new ClassNotFound();
+
+            return result;
+        }
+    }
+}
