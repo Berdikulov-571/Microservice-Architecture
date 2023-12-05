db27efab548ac3d768683d4939f7454f29a4f457
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:48:28 +0500

    Teachers Changes

--- src/School.Platform/School.Service/Abstractions/UseCases/Teachers/Queries/Get/GetTeacherImageQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace School.Service.Abstractions.UseCases.Teachers.Queries.Get
-{
-    public class GetTeacherImageQuery : IRequest<byte[]>
-    {
-        public int TeacherId { get; set; }
-    }
-}
