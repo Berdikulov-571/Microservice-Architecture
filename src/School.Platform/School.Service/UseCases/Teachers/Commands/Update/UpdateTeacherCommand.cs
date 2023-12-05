fefd8670351c12194278d8cd94232a15f4747364
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:42:35 +0500

    Update Teacher Command

---
+++ src/School.Platform/School.Service/UseCases/Teachers/Commands/Update/UpdateTeacherCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Dtos.Teachers;
+
+namespace School.Service.UseCases.Teachers.Commands.Update
+{
+    public class UpdateTeacherCommand : TeacherUpdateDto, IRequest<int>
+    {
+
+    }
+}
