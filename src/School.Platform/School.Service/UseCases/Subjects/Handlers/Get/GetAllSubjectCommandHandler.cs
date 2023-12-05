2f1f49dc61e09675fd89ac39afe8303cd32bc9d7
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:45:36 +0500

    Get All Subjects Command Handler

---
+++ src/School.Platform/School.Service/UseCases/Subjects/Handlers/Get/GetAllSubjectCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Subjects;
+using School.Domain.Exceptions.Subjects;
+using School.Service.Abstractions.DataAccess;
+using School.Service.UseCases.Subjects.Queries.Get;
+
+namespace School.Service.UseCases.Subjects.Handlers.Get
+{
+    public class GetAllSubjectCommandHandler : IRequestHandler<GetAllSubjecttQuery, IEnumerable<Subject>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllSubjectCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Subject>> Handle(GetAllSubjecttQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Subject> subjects = await _context.Subjects.ToListAsync(cancellationToken);
+
+            if (subjects == null)
+                throw new SubjectNotFound();
+
+            return subjects;
+        }
+    }
+}
