1817ea7f35fd31cde5c1594ba111f8f24b4609a9
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 11:07:06 +0500

    DeleteClass

--- src/School.Platform/School.Service/Abstractions/UseCases/Classes/Handlers/Delete/DeleteClassCommandHandler.cs
+++ src/School.Platform/School.Service/Abstractions/UseCases/Classes/Handlers/Delete/DeleteClassCommandHandler.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Classes;
+using School.Domain.Exceptions.Classes;
+using School.Service.Abstractions.DataAccess;
+using School.Service.Abstractions.UseCases.Classes.Commands.Delete;
 
 namespace School.Service.Abstractions.UseCases.Classes.Handlers.Delete
 {
-    internal class DeleteClassCommandHandler
+    public class DeleteClassCommandHandler : IRequestHandler<DeleteClassCommand, int>
     {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteClassCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteClassCommand request, CancellationToken cancellationToken)
+        {
+            Class? @class = await _context.Classes.FirstOrDefaultAsync(x => x.ClassId == request.ClassId,cancellationToken);
+
+            if (@class == null)
+                throw new ClassNotFound();
+
+            _context.Classes.Remove(@class);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
     }
 }
