0e0a08a032593fc6dcf8059589942cc9c6d2d654
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 12:06:29 +0500

    GetStudentImageQuery

---
+++ src/School.Platform/School.Service/Abstractions/UseCases/Students/Queries/Get/GetStudentImageQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace School.Service.Abstractions.UseCases.Students.Queries.Get
+{
+    public class GetStudentImageQuery : IRequest<byte[]>
+    {
+        public int StudentId { get; set; }
+    }
+}
