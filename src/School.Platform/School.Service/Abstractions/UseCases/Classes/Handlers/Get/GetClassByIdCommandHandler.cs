448c2dd4c6263d8160ae353b9c7a65b9b1f4c401
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 11:07:48 +0500

    GetClassByIdCommandHandler

--- src/School.Platform/School.Service/Abstractions/UseCases/Classes/Handlers/Get/GetClassByIdCommandHandler.cs
+++ src/School.Platform/School.Service/Abstractions/UseCases/Classes/Handlers/Get/GetClassByIdCommandHandler.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Classes;
+using School.Domain.Exceptions.Classes;
+using School.Service.Abstractions.DataAccess;
+using School.Service.Abstractions.UseCases.Classes.Queries.Get;
 
 namespace School.Service.Abstractions.UseCases.Classes.Handlers.Get
 {
-    internal class GetClassByIdCommandHandler
+    public class GetClassByIdCommandHandler : IRequestHandler<GetClassByIdQuery, Class>
     {
+        private readonly IApplicationDbContext _context;
+
+        public GetClassByIdCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Class> Handle(GetClassByIdQuery request, CancellationToken cancellationToken)
+        {
+            Class? @class = await _context.Classes.FirstOrDefaultAsync(x => x.ClassId == request.ClassId, cancellationToken);
+
+            if (@class == null)
+                throw new ClassNotFound();
+
+            return @class;
+        }
     }
 }
