176fa4f45991e9844fb13324186fa7bea50cea9b
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 12:05:15 +0500

    StudentUpdateDto

--- src/School.Platform/School.Domain/Dtos/Students/StudentUpdateDto.cs
+++ src/School.Platform/School.Domain/Dtos/Students/StudentUpdateDto.cs
@@ -16,7 +16,6 @@ namespace School.Domain.Dtos.Students
         public string Address { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public Role Role { get; set; }
         public IFormFile? Image { get; set; }
         public int ClassId { get; set; }
     }
