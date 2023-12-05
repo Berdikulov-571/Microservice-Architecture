53d00376773608e84c38141fd936b04e50bf3ad2
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:44:59 +0500

    Get Subject Queries

---
+++ src/School.Platform/School.Service/UseCases/Subjects/Queries/Get/GetAllSubjecttQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Entities.Subjects;
+
+namespace School.Service.UseCases.Subjects.Queries.Get
+{
+    public class GetAllSubjecttQuery : IRequest<IEnumerable<Subject>>
+    {
+
+    }
+}
