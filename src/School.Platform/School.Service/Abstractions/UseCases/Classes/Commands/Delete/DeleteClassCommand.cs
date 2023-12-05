775c6e461a79277f9ee2229124ace339eb363f58
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 11:06:12 +0500

    DeleteClassCOmmandHandler

--- src/School.Platform/School.Service/Abstractions/UseCases/Classes/Commands/Delete/DeleteClassCommand.cs
+++ src/School.Platform/School.Service/Abstractions/UseCases/Classes/Commands/Delete/DeleteClassCommand.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace School.Service.Abstractions.UseCases.Classes.Commands.Delete
 {
-    internal class DeleteClassCommand
+    public class DeleteClassCommand : IRequest<int>
     {
+        public int ClassId { get; set; }
     }
 }
