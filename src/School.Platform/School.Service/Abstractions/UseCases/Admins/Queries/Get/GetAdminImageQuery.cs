ceea3fc03364c27ae6fc190f719f8134338c5700
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 11:26:39 +0500

    GetAdminImageQuery

---
+++ src/School.Platform/School.Service/Abstractions/UseCases/Admins/Queries/Get/GetAdminImageQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace School.Service.Abstractions.UseCases.Admins.Queries.Get
+{
+    public class GetAdminImageQuery : IRequest<byte[]>
+    {
+        public int AdminId { get; set; }
+    }
+}
