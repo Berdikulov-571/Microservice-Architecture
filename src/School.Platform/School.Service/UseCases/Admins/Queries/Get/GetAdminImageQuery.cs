4d1d01194e35585f7c0f773e1f983ffab978b29c
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:47:22 +0500

    Admin Image Query

---
+++ src/School.Platform/School.Service/UseCases/Admins/Queries/Get/GetAdminImageQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace School.Service.UseCases.Admins.Queries.Get
+{
+    public class GetAdminImageQuery : IRequest<byte[]>
+    {
+        public int AdminId { get; set; }
+    }
+}
