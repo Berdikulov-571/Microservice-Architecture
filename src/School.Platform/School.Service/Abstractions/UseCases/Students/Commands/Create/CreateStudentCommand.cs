1f77b6b1f7f2f61dc4d2cc5907e68bfcdb2971b2
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 11:08:06 +0500

    CreateStudentCOmmand

--- src/School.Platform/School.Service/Abstractions/UseCases/Students/Commands/Create/CreateStudentCommand.cs
+++ src/School.Platform/School.Service/Abstractions/UseCases/Students/Commands/Create/CreateStudentCommand.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace School.Service.Abstractions.UseCases.Students.Commands.Create
+﻿namespace School.Service.Abstractions.UseCases.Students.Commands.Create
 {
-    internal class CreateStudentCommand
+    public class CreateStudentCommand
     {
     }
 }
