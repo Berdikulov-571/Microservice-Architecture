6cfc0a43d30523567b1eeb7cc3a2eabe36df9bab
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:45:23 +0500

    Update Subejct Command Handler

---
+++ src/School.Platform/School.Service/UseCases/Subjects/Handlers/Update/UpdateSubjectCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Subjects;
+using School.Domain.Exceptions.Subjects;
+using School.Service.Abstractions.DataAccess;
+using School.Service.UseCases.Subjects.Commands.Update;
+
+namespace School.Service.UseCases.Subjects.Handlers.Update
+{
+    public class UpdateSubjectCommandHandler : IRequestHandler<UpdateSubjectCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateSubjectCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateSubjectCommand request, CancellationToken cancellationToken)
+        {
+            Subject? subject = await _context.Subjects.FirstOrDefaultAsync(x => x.SubjectId == request.SubjectId);
+
+            if (subject == null)
+                throw new SubjectNotFound();
+
+            subject.Name = request.Name;
+
+            _context.Subjects.Update(subject);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
