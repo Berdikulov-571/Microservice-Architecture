d7ab05cba6a913db732645044f89a9bded9b73ae
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 12:07:30 +0500

    DeleteStudentCommand

--- src/School.Platform/School.Service/Abstractions/UseCases/Students/Commands/Delete/DeleteStudentCommand.cs
+++ src/School.Platform/School.Service/Abstractions/UseCases/Students/Commands/Delete/DeleteStudentCommand.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace School.Service.Abstractions.UseCases.Students.Commands.Delete
 {
-    internal class DeleteStudentCommand
+    public class DeleteStudentCommand : IRequest<int>
     {
+        public int StudentId { get; set; }
     }
 }
