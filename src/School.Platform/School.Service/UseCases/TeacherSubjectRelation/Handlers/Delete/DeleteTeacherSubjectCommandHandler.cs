ecd5f9369fffe997e5aa20ef897ed754f7e4bc54
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:40:46 +0500

    DeleteTeacherCommandHandler

---
+++ src/School.Platform/School.Service/UseCases/TeacherSubjectRelation/Handlers/Delete/DeleteTeacherSubjectCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.TeacherSubjectRelation;
+using School.Domain.Exceptions.TeacherSubject;
+using School.Service.Abstractions.DataAccess;
+using School.Service.UseCases.TeacherSubjectRelation.Commands.Delete;
+
+namespace School.Service.UseCases.TeacherSubjectRelation.Handlers.Delete
+{
+    public class DeleteTeacherSubjectCommandHandler : IRequestHandler<DeleteTeacherSubjectCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteTeacherSubjectCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteTeacherSubjectCommand request, CancellationToken cancellationToken)
+        {
+            TeacherSubjects? teacherSubject = await _context.TeacherSubjects.FirstOrDefaultAsync(x => x.TeacherSubjectId == request.TeacherSubjectId);
+
+            if (teacherSubject == null)
+                throw new TeacherSubjectNotFound();
+
+            _context.TeacherSubjects.Remove(teacherSubject);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
