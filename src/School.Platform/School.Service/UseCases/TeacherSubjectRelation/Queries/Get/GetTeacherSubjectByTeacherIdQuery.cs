284d0d8ed4a10c7fc6d2fef54dee4e27318f6efa
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:39:38 +0500

    GetAllTEacherSubjectQuery

---
+++ src/School.Platform/School.Service/UseCases/TeacherSubjectRelation/Queries/Get/GetTeacherSubjectByTeacherIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Entities.Subjects;
+
+namespace School.Service.UseCases.TeacherSubjectRelation.Queries.Get
+{
+    public class GetTeacherSubjectByTeacherIdQuery : IRequest<IEnumerable<Subject>>
+    {
+        public int TeacherId { get; set; }
+    }
+}
