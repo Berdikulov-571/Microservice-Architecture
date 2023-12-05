64c55461362470db5ba9d7f8647322e0e933ae0b
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:42:04 +0500

    Get Teacher Command Handler

---
+++ src/School.Platform/School.Service/UseCases/Teachers/Handlers/Get/GetAllTeacherCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Teachers;
+using School.Domain.Exceptions.Teacher;
+using School.Service.Abstractions.DataAccess;
+using School.Service.UseCases.Teachers.Queries.Get;
+
+namespace School.Service.UseCases.Teachers.Handlers.Get
+{
+    public class GetAllTeacherCommandHandler : IRequestHandler<GetAllTeacherQuery, IEnumerable<Teacher>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllTeacherCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Teacher>> Handle(GetAllTeacherQuery request, CancellationToken cancellationToken)
+        {
+            List<Teacher> teachers = await _context.Teachers.ToListAsync(cancellationToken);
+
+            if (teachers == null)
+                throw new TeacherNotFound();
+
+            return teachers;
+        }
+    }
+}
