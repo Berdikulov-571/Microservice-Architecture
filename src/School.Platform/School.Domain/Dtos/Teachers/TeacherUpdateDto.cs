91ca7e1a3f59e94f3a2605663ca1dc0387c30514
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 12:05:52 +0500

    TeacherUpdateDto

--- src/School.Platform/School.Domain/Dtos/Teachers/TeacherUpdateDto.cs
+++ src/School.Platform/School.Domain/Dtos/Teachers/TeacherUpdateDto.cs
@@ -16,7 +16,6 @@ namespace School.Domain.Dtos.Teachers
         public string Address { get; set; }
         public string PasswordHash { get; set; }
         public string Email { get; set; }
-        public Role Role { get; set; }
         public IFormFile ImagePath { get; set; }
     }
 }
