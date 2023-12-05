84b00f356ed1114fb473b0cf6f799bbdb0bec8ac
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:41:39 +0500

    Get Teacher Queries

---
+++ src/School.Platform/School.Service/UseCases/Teachers/Queries/Get/GetAllTeacherQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Entities.Teachers;
+
+namespace School.Service.UseCases.Teachers.Queries.Get
+{
+    public class GetAllTeacherQuery : IRequest<IEnumerable<Teacher>>
+    {
+
+    }
+}
