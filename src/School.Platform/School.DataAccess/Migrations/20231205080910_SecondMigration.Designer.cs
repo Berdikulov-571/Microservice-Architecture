46003bf75ab9aa4054d7df3e81717587a8b4bd47
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 13:11:21 +0500

    Migrations

--- src/School.Platform/School.DataAccess/Migrations/20231205080910_SecondMigration.Designer.cs
+++ src/School.Platform/School.DataAccess/Migrations/20231205080910_SecondMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using School.DataAccess.Persistence.DataContexts;
 
@@ -11,9 +12,11 @@ using School.DataAccess.Persistence.DataContexts;
 namespace School.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231205080910_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
