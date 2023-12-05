c31bd9bc8afe0e4bb5b9871f3cee8045d0c5718f
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 11:07:16 +0500

    TeacherAlredayExists

---
+++ src/School.Platform/School.Domain/Exceptions/GlobalExceptions/AlreadyExistsExceptions/TeacherAlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace School.Domain.Exceptions.GlobalExceptions.AlreadyExistsExceptions
+{
+    public class TeacherAlreadyExistsException : Exception
+    {
+        private HttpStatusCode StatusCode { get; } = HttpStatusCode.Forbidden;
+        private string TitleMessage { get; set; } = default!;
+
+        public TeacherAlreadyExistsException()
+        {
+            TitleMessage = "This Teacher Already Exists !";
+        }
+    }
+}
