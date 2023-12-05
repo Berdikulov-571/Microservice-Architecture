012391fccd75acf9ad335ed3ec5f7e9d35e5fe37
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:46:49 +0500

    Delete Admin Command Handler

---
+++ src/School.Platform/School.Service/UseCases/Admins/Handlers/Delete/DeleteAdminCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Admins;
+using School.Domain.Exceptions.Admins;
+using School.Domain.Exceptions.Image;
+using School.Service.Abstractions.DataAccess;
+using School.Service.Interfaces.File;
+using School.Service.UseCases.Admins.Commands.Delete;
+
+namespace School.Service.UseCases.Admins.Handlers.Delete
+{
+    public class DeleteAdminCommandHandler : IRequestHandler<DeleteAdminCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IFileService _fileService;
+
+        public DeleteAdminCommandHandler(IApplicationDbContext context, IFileService fileService)
+        {
+            _context = context;
+            _fileService = fileService;
+        }
+
+        public async Task<int> Handle(DeleteAdminCommand request, CancellationToken cancellationToken)
+        {
+            Admin? admin = await _context.Admins.FirstOrDefaultAsync(x => x.AdminId == request.AdminId, cancellationToken);
+
+            if (admin == null)
+            {
+                throw new AdminNotFound();
+            }
+
+            try
+            {
+                await _fileService.DeleteImageAsync(admin.ImagePath);
+            }
+            catch
+            {
+                throw new ImageNotFound();
+            }
+            _context.Admins.Remove(admin);
+
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
+
