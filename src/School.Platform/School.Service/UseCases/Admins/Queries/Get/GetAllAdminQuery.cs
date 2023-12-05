ec93bc35fafaf7eb75f69d1abad66df3d7f948f2
berdikulov-571 <mygithub571@gmail.com>
2023-12-05 21:47:14 +0500

    Get All Query

---
+++ src/School.Platform/School.Service/UseCases/Admins/Queries/Get/GetAllAdminQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Entities.Admins;
+
+namespace School.Service.UseCases.Admins.Queries.Get
+{
+    public class GetAllAdminQuery : IRequest<IEnumerable<Admin>>
+    {
+
+    }
+}
