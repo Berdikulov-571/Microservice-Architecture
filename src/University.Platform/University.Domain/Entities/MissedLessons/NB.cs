e4206ee05edc624873152ebb9a3c29ffedb1e222
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:10:21 +0500

    MissedLessons

---
+++ src/University.Platform/University.Domain/Entities/MissedLessons/NB.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using University.Domain.Common.BaseEntities;
+using University.Domain.Entities.Lessons;
+using University.Domain.Entities.Students;
+
+namespace University.Domain.Entities.MissedLessons
+{
+    public class NB : Auditable
+    {
+        [Key]
+        public int NbId { get; set; }
+        public bool IsAvailable { get; set; }
+        [ForeignKey(nameof(Lesson))]
+        public int LessonId { get; set; }
+        [ForeignKey(nameof(Student))]
+        public int StudentId { get; set; }
+
+        public Lesson? Lesson { get; set; }
+        public Student? Student { get; set; }
+    }
+}
