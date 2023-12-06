8596971303df8fe806ef400ac3d393570dd5f348
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:09:59 +0500

    Admins

---
+++ src/University.Platform/University.Domain/Entities/Admins/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using University.Domain.Common.BaseEntities;
+using University.Domain.Enums.Roles;
+
+namespace University.Domain.Entities.Admins
+{
+    public class Admin : Auditable
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
