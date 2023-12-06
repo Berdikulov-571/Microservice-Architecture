591a6911022ba33b30e0d552319c8cc68aeb8ea5
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:40 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Groups/Queries/Get/GetAllGroupQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Entities.Groups;
+
+namespace University.Service.UseCases.Groups.Queries.Get
+{
+    public class GetAllGroupQuery : IRequest<IEnumerable<Group>>
+    {
+    }
+}
