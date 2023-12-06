1d0fa63de0cf5c23dbb7d67fd2290f2767fbd77b
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:46 +0500

    Delete

---
+++ src/University.Platform/University.Service/UseCases/Lessons/Commands/Delete/DeleteLessonCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace University.Service.UseCases.Lessons.Commands.Delete
+{
+    public class DeleteLessonCommand : IRequest<int>
+    {
+        public int LessonId { get; set; }
+    }
+}
