3c9f46792e8a1f6caf834bb5e68d1339a9c1f2b4
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:08:29 +0500

    PhoneNumberAttribute

---
+++ src/University.Platform/University.Domain/Attributes/PhoneNumber/PhoneNumberAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University.Domain.Attributes.PhoneNumber
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class PhoneNumberAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            string phoneNumber = value.ToString();
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(phoneNumber, @"^\+998\d{9}$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
