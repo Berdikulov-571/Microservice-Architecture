1936c09af39cc39aba60d6b0b90d030e119ddd5f
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:20:09 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Lessons/Queries/Get/GetAllLessonsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Entities.Lessons;
+
+namespace University.Service.UseCases.Lessons.Queries.Get
+{
+    public class GetAllLessonsQuery : IRequest<IEnumerable<Lesson>>
+    {
+    }
+}
---
+++ src/University.Platform/University.Service/UseCases/Lessons/Queries/Get/GetLessonByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Entities.Lessons;
+
+namespace University.Service.UseCases.Lessons.Queries.Get
+{
+    public class GetLessonByIdQuery : IRequest<Lesson>
+    {
+        public int LessonId { get; set; }
+    }
+}
