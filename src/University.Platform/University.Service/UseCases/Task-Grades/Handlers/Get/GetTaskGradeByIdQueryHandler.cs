af9c3f9de233c22f9a6d7c5b4d178ddb228cb15a
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 20:48:56 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Task-Grades/Handlers/Get/GetTaskGradeByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Task_Grades;
+using University.Domain.Exceptions.Task_Grade;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Task_Grades.Queries.Get;
+
+namespace University.Service.UseCases.Task_Grades.Handlers.Get
+{
+    public class GetTaskGradeByIdQueryHandler : IRequestHandler<GetTaskGradeByIdQuery, TaskGrade>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetTaskGradeByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TaskGrade> Handle(GetTaskGradeByIdQuery request, CancellationToken cancellationToken)
+        {
+            TaskGrade? taskGrade = await _context.TaskGrades.FirstOrDefaultAsync(x => x.TaskGradeId == request.TaskGradeId,cancellationToken);
+
+            if (taskGrade == null)
+                throw new TaskGradeNotFound();
+
+            return taskGrade;
+        }
+    }
+}
