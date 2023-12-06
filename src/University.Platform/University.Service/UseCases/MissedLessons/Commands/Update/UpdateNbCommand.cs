abf1653088bbcbebf670218eee318d6871c1180f
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 20:48:30 +0500

    Update

---
+++ src/University.Platform/University.Service/UseCases/MissedLessons/Commands/Update/UpdateNbCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Dtos.MissedLessons;
+
+namespace University.Service.UseCases.MissedLessons.Commands.Update
+{
+    public class UpdateNbCommand : NbUpdateDto, IRequest<int>
+    {
+
+    }
+}
