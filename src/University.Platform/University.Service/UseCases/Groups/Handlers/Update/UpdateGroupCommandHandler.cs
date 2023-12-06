0c27ab0a2c1c3b10a48b5efd27d515f1cc6b3d1e
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:21 +0500

    Update

---
+++ src/University.Platform/University.Service/UseCases/Groups/Handlers/Update/UpdateGroupCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Groups;
+using University.Domain.Exceptions.Groups;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Groups.Commands.Update;
+
+namespace University.Service.UseCases.Groups.Handlers.Update
+{
+    public class UpdateGroupCommandHandler : IRequestHandler<UpdateGroupCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateGroupCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateGroupCommand request, CancellationToken cancellationToken)
+        {
+            Group? group = await _context.Groups.FirstOrDefaultAsync(x => x.GroupId == request.GroupId,cancellationToken);
+
+            if (group == null)
+                throw new GroupNotFound();
+
+            group.Level = request.Level;
+            group.Name = request.Name;
+            group.UpdateAt = DateTime.Now;
+
+            _context.Groups.Update(group);
+
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
