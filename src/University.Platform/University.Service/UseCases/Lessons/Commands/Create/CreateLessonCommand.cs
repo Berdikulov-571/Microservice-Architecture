7ceb0b5f1edb455e160ffec8226e08f10b604b74
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:34 +0500

    Create

---
+++ src/University.Platform/University.Service/UseCases/Lessons/Commands/Create/CreateLessonCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Dtos.Lessons;
+
+namespace University.Service.UseCases.Lessons.Commands.Create
+{
+    public class CreateLessonCommand : LessonCreateDto, IRequest<int>
+    {
+
+    }
+}
