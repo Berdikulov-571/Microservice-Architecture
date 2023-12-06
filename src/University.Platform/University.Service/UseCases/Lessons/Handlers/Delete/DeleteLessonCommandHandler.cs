1b04877c45e84fc19e14cc976ea28a922ed30785
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:22:20 +0500

    Delete

---
+++ src/University.Platform/University.Service/UseCases/Lessons/Handlers/Delete/DeleteLessonCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Lessons;
+using University.Domain.Exceptions.Lessons;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Lessons.Commands.Delete;
+
+namespace University.Service.UseCases.Lessons.Handlers.Delete
+{
+    public class DeleteLessonCommandHandler : IRequestHandler<DeleteLessonCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteLessonCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteLessonCommand request, CancellationToken cancellationToken)
+        {
+            Lesson? lesson = await _context.Lessons.FirstOrDefaultAsync(x => x.LessonId == request.LessonId, cancellationToken);
+
+            if (lesson == null)
+                throw new LessonNotFound();
+
+            _context.Lessons.Remove(lesson);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
