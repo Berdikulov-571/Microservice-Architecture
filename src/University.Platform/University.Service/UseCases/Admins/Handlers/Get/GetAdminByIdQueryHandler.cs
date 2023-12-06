21b282f49234d4ebe02c724d1008656af9bd766c
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:15:53 +0500

    Get Handler

---
+++ src/University.Platform/University.Service/UseCases/Admins/Handlers/Get/GetAdminByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Admins;
+using University.Domain.Exceptions.Admins;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Admins.Queries.Get;
+
+namespace University.Service.UseCases.Admins.Handlers.Get
+{
+    public class GetAdminByIdQueryHandler : IRequestHandler<GetAdminByIdQuery, Admin>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAdminByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Admin> Handle(GetAdminByIdQuery request, CancellationToken cancellationToken)
+        {
+            Admin? admin = await _context.Admins.FirstOrDefaultAsync(x => x.AdminId == request.AdminId, cancellationToken);
+
+            if (admin == null)
+                throw new AdminNotFound();
+
+            return admin;
+        }
+    }
+}
---
+++ src/University.Platform/University.Service/UseCases/Admins/Handlers/Get/GetAllAdminQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Admins;
+using University.Domain.Exceptions.Admins;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Admins.Queries.Get;
+
+namespace University.Service.UseCases.Admins.Handlers.Get
+{
+    public class GetAllAdminQueryHandler : IRequestHandler<GetAllAdminQuery, IEnumerable<Admin>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllAdminQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Admin>> Handle(GetAllAdminQuery request, CancellationToken cancellationToken)
+        {
+            List<Admin> admins = await _context.Admins.ToListAsync(cancellationToken);
+
+            if (admins == null)
+                throw new AdminNotFound();
+
+            return admins;
+        }
+    }
+}
