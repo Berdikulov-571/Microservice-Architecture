99f7e83564eca9fd14bd686940f0f31dc5eb7fc7
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:08:45 +0500

    Courses

---
+++ src/University.Platform/University.Domain/Dtos/Courses/CourseCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace University.Domain.Dtos.Courses
+{
+    public class CourseCreateDto
+    {
+        public string Name { get; set; }
+        public int TeacherId { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
---
+++ src/University.Platform/University.Domain/Dtos/Courses/CourseUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Dtos.Courses
+{
+    public class CourseUpdateDto
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public int TeacherId { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
