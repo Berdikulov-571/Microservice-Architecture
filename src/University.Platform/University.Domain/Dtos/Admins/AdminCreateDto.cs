65a609114fbb6ac9d8af455f5d93eb33c55dd84b
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:08:39 +0500

    Admin Dto

---
+++ src/University.Platform/University.Domain/Dtos/Admins/AdminCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace University.Domain.Dtos.Admins
+{
+    public class AdminCreateDto
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public IFormFile ImagePath { get; set; }
+    }
+}
