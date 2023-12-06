dde088ab86dd0c4195587c2814bac8b6d79dcce9
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:15 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Groups/Handlers/Get/GetAllGroupQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Groups;
+using University.Domain.Exceptions.Groups;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Groups.Queries.Get;
+
+namespace University.Service.UseCases.Groups.Handlers.Get
+{
+    public class GetAllGroupQueryHandler : IRequestHandler<GetAllGroupQuery, IEnumerable<Group>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllGroupQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Group>> Handle(GetAllGroupQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Group> groups = await _context.Groups.ToListAsync(cancellationToken);
+
+            if (groups == null)
+                throw new GroupNotFound();
+
+            return groups;
+        }
+    }
+}
