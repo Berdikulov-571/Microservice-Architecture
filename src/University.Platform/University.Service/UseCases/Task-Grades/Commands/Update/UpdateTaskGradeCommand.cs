fda5b10dfe51475de131fbbb879cbdbd853ef25a
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 20:48:37 +0500

    Update

---
+++ src/University.Platform/University.Service/UseCases/Task-Grades/Commands/Update/UpdateTaskGradeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Dtos.Task_Grades;
+
+namespace University.Service.UseCases.Task_Grades.Commands.Update
+{
+    public class UpdateTaskGradeCommand : TaskGradeUpdateDto, IRequest<int>
+    {
+
+    }
+}
