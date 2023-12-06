71d8929ea6b4f8b4567f0be5db5d9f58b08adbe6
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:07:33 +0500

    Migrations

--- src/University.Platform/University.DataAccess/Migrations/20231206121940_Seven.Designer.cs
+++ src/University.Platform/University.DataAccess/Migrations/20231206121940_Seven.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using University.DataAccess.Persistence.DataContexts;
 
@@ -11,9 +12,11 @@ using University.DataAccess.Persistence.DataContexts;
 namespace University.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231206121940_Seven")]
+    partial class Seven
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
