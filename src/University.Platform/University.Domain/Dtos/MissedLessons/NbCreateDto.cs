15fa6768dd7306b3a1cbebf4ea8b2608798a22f5
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 20:49:20 +0500

    Nb

--- src/University.Platform/University.Domain/Dtos/MissedLessons/NbCreateDto.cs
+++ src/University.Platform/University.Domain/Dtos/MissedLessons/NbCreateDto.cs
@@ -2,7 +2,6 @@
 {
     public class NbCreateDto
     {
-        public int NbId { get; set; }
         public bool IsAvailable { get; set; }
         public int LessonId { get; set; }
         public int StudentId { get; set; }
