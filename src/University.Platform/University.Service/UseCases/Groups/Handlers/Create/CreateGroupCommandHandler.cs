02046ce54f7b464af57fb5957ae4b90e64139f80
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:10 +0500

    Create

---
+++ src/University.Platform/University.Service/UseCases/Groups/Handlers/Create/CreateGroupCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using University.Domain.Entities.Groups;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Groups.Commands.Create;
+
+namespace University.Service.UseCases.Groups.Handlers.Create
+{
+    public class CreateGroupCommandHandler : IRequestHandler<CreateGroupCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateGroupCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateGroupCommand request, CancellationToken cancellationToken)
+        {
+            Group group = new Group()
+            {
+                Level = request.Level,
+                Name = request.Name,
+                CreatedAt = DateTime.Now,
+            };
+
+            await _context.Groups.AddAsync(group, cancellationToken);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
