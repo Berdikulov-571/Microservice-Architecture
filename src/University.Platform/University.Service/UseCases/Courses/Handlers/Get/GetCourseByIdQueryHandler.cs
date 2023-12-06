395f2f09b51edec3953c671a26c98e60678e700e
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:18:21 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Courses/Handlers/Get/GetCourseByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Courses;
+using University.Domain.Exceptions.Courses;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Courses.Queries.Get;
+
+namespace University.Service.UseCases.Courses.Handlers.Get
+{
+    public class GetCourseByIdQueryHandler : IRequestHandler<GetCourseByIdQuery, Course>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetCourseByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Course> Handle(GetCourseByIdQuery request, CancellationToken cancellationToken)
+        {
+            Course? course = await _context.Courses.FirstOrDefaultAsync(x => x.CourseId == request.CourseId,cancellationToken);
+
+            if (course == null)
+                throw new CourseNotFound();
+
+            return course;
+        }
+    }
+}
