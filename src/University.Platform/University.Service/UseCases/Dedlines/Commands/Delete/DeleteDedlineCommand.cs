9716d43210c65cbeb6b5759f259fe1fc70889f6e
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:18:06 +0500

    Delete

---
+++ src/University.Platform/University.Service/UseCases/Dedlines/Commands/Delete/DeleteDedlineCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace University.Service.UseCases.Dedlines.Commands.Delete
+{
+    public class DeleteDedlineCommand : IRequest<int>
+    {
+        public int DedlineId { get; set; }
+    }
+}
