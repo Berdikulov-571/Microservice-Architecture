e7ca9245f31af5657e91cb4a9bf71093481eb8ab
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:17:38 +0500

    Get QUeries

---
+++ src/University.Platform/University.Service/UseCases/Teachers/Handlers/Get/GetAllTeachersQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Teachers;
+using University.Domain.Exceptions.Teachers;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Teachers.Queries.Get;
+
+namespace University.Service.UseCases.Teachers.Handlers.Get
+{
+    public class GetAllTeachersQueryHandler : IRequestHandler<GetAllTeacherQuery, IEnumerable<Teacher>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllTeachersQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Teacher>> Handle(GetAllTeacherQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Teacher> teachers = await _context.Teachers.ToListAsync(cancellationToken);
+
+            if (teachers == null)
+                throw new TeacherNotFound();
+
+            return teachers;
+        }
+    }
+}
---
+++ src/University.Platform/University.Service/UseCases/Teachers/Handlers/Get/GetTeacherbyIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Teachers;
+using University.Domain.Exceptions.Teachers;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Teachers.Queries.Get;
+
+namespace University.Service.UseCases.Teachers.Handlers.Get
+{
+    public class GetTeacherbyIdQueryHandler : IRequestHandler<GetTeacherByIdQuery, Teacher>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetTeacherbyIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Teacher> Handle(GetTeacherByIdQuery request, CancellationToken cancellationToken)
+        {
+            Teacher? teacher = await _context.Teachers.FirstOrDefaultAsync(x => x.TeacherId == request.TeacherId,cancellationToken);
+
+            if (teacher == null)
+                throw new TeacherNotFound();
+
+            return teacher;
+        }
+    }
+}
