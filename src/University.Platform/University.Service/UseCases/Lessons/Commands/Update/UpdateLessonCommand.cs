e0540485296487e7f26dfae690fb35a2c16264ad
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:22:51 +0500

    Update

---
+++ src/University.Platform/University.Service/UseCases/Lessons/Commands/Update/UpdateLessonCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Dtos.Lessons;
+
+namespace University.Service.UseCases.Lessons.Commands.Update
+{
+    public class UpdateLessonCommand : LessonUpdateDto, IRequest<int>
+    {
+    }
+}
