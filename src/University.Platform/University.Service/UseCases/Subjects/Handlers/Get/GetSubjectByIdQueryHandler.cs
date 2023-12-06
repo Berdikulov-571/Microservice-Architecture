67814a4dcf7236c53bc6327c98223d59bc41276e
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:20:32 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Subjects/Handlers/Get/GetSubjectByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Subjects;
+using University.Domain.Exceptions.Subjects;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Subjects.Queries.Get;
+
+namespace University.Service.UseCases.Subjects.Handlers.Get
+{
+    public class GetSubjectByIdQueryHandler : IRequestHandler<GetSubjectByIdQuery, Subject>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetSubjectByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Subject> Handle(GetSubjectByIdQuery request, CancellationToken cancellationToken)
+        {
+            Subject? subject = await _context.Subjects.FirstOrDefaultAsync(x => x.SubjectId == request.SubjectId,cancellationToken);
+
+            if (subject == null)
+                throw new SubjectNotFound();
+
+            return subject;
+        }
+    }
+}
