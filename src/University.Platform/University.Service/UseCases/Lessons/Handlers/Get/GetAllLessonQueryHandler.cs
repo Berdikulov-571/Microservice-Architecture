50c30a79dd6340c0261f1b267f4372bcb4f99038
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:19:58 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Lessons/Handlers/Get/GetAllLessonQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Lessons;
+using University.Domain.Exceptions.Lessons;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Lessons.Queries.Get;
+
+namespace University.Service.UseCases.Lessons.Handlers.Get
+{
+    public class GetAllLessonQueryHandler : IRequestHandler<GetAllLessonsQuery, IEnumerable<Lesson>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllLessonQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Lesson>> Handle(GetAllLessonsQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Lesson> lessons = await _context.Lessons.ToListAsync(cancellationToken);
+
+            if (lessons == null)
+                throw new LessonNotFound();
+
+            return lessons;
+        }
+    }
+}
---
+++ src/University.Platform/University.Service/UseCases/Lessons/Handlers/Get/GetLessonByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Lessons;
+using University.Domain.Exceptions.Lessons;
+using University.Service.Abstractions.DataContexts;
+using University.Service.UseCases.Lessons.Queries.Get;
+
+namespace University.Service.UseCases.Lessons.Handlers.Get
+{
+    public class GetLessonByIdQueryHandler : IRequestHandler<GetLessonByIdQuery, Lesson>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetLessonByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Lesson> Handle(GetLessonByIdQuery request, CancellationToken cancellationToken)
+        {
+            Lesson? lesson = await _context.Lessons.FirstOrDefaultAsync(x => x.LessonId == request.LessonId, cancellationToken);
+
+            if (lesson == null)
+                throw new LessonNotFound();
+
+            return lesson;
+        }
+    }
+}
