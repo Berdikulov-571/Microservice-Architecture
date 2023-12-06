5d7bb42c86158617ae23d21a0f19f4ce9957710c
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:12:55 +0500

    Student

---
+++ src/University.Platform/University.Domain/Exceptions/Students/StudentNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.Students
+{
+    public class StudentNotFound : GlobalException
+    {
+        public StudentNotFound()
+        {
+            TitleMessage = "Student Not Found !";
+        }
+    }
+}
