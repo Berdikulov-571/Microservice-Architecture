4eea740b32211533835077ddb03c570493be5d4f
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:20:37 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/Subjects/Queries/Get/GetSubjectByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Entities.Subjects;
+
+namespace University.Service.UseCases.Subjects.Queries.Get
+{
+    public class GetSubjectByIdQuery : IRequest<Subject>
+    {
+        public int SubjectId { get; set; }
+    }
+}
