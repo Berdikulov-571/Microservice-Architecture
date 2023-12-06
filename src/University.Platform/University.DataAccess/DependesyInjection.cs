0741e7d789dbf77b8e620af2a8cc91841a417cee
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:07:49 +0500

    Dependecy Injection

---
+++ src/University.Platform/University.DataAccess/DependesyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using University.DataAccess.Persistence.DataContexts;
+using University.Service.Abstractions.DataContexts;
+
+namespace University.DataAccess
+{
+    public static class DependesyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
