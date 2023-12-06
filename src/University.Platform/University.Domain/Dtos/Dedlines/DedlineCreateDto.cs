c3897a6c826dfc61c86ba379c9701d9a2574f749
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:08:52 +0500

    Dedline

---
+++ src/University.Platform/University.Domain/Dtos/Dedlines/DedlineCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace University.Domain.Dtos.Dedlines
+{
+    public class DedlineCreateDto
+    {
+        public float MaxGrade { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public IFormFile FilePath { get; set; }
+        public int CourseId { get; set; }
+    }
+}
---
+++ src/University.Platform/University.Domain/Dtos/Dedlines/DedlineUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace University.Domain.Dtos.Dedlines
+{
+    public class DedlineUpdateDto
+    {
+        public int DedlineId { get; set; }
+        public float MaxGrade { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public IFormFile? FilePath { get; set; }
+        public int CourseId { get; set; }
+    }
+}
