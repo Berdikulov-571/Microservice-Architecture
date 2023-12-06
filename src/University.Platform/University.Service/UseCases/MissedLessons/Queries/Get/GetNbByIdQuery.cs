4830314061b79161c69567566b86bdc359bfce07
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 20:47:53 +0500

    Get

---
+++ src/University.Platform/University.Service/UseCases/MissedLessons/Queries/Get/GetNbByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Entities.MissedLessons;
+
+namespace University.Service.UseCases.MissedLessons.Queries.Get
+{
+    public class GetnbByIdQuery : IRequest<NB>
+    {
+        public int NbId { get; set; }
+    }
+}
