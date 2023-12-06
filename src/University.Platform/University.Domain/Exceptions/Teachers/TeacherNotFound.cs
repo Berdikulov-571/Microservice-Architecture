972ba682b0636f373ad250f492d3e0c4efa6997c
berdikulov-571 <mygithub571@gmail.com>
2023-12-06 19:12:49 +0500

    Teachers

---
+++ src/University.Platform/University.Domain/Exceptions/Teachers/TeacherNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.Teachers
+{
+    public class TeacherNotFound : GlobalException
+    {
+        public TeacherNotFound()
+        {
+            TitleMessage = "Teacher Not Found !s";
+        }
+    }
+}
