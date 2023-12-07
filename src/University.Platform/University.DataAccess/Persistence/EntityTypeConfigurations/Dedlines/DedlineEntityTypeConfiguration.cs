be113104e8973e2704447b09400ca32de811066a
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 15:13:45 +0500

    Configuration

---
+++ src/University.Platform/University.DataAccess/Persistence/EntityTypeConfigurations/Dedlines/DedlineEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+using University.Domain.Entities.Dedlines;
+
+namespace University.DataAccess.Persistence.EntityTypeConfigurations.Dedlines
+{
+    public class DedlineEntityTypeConfiguration : IEntityTypeConfiguration<Dedline>
+    {
+        public void Configure(EntityTypeBuilder<Dedline> builder)
+        {
+            builder.HasKey(d => d.DedlineId);
+
+            builder.HasOne(d => d.Course)
+                .WithMany(c => c.Deadlines)
+                .HasForeignKey(d => d.CourseId);
+        }
+    }
+}
