fef201318644d14d26e0d835c5c0d38cfa904984
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:02:29 +0500

    Percel Create

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/Parcels/Commands/Create/ParcelCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using Kadastr.Domain.Dtos.Parcels;
+using MediatR;
+
+namespace Kadastr.Service.UseCases.Parcels.Commands.Create
+{
+    public class ParcelCreateCommand : ParcelCreateDto, IRequest<int>
+    {
+    }
+}
