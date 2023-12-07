0ef03f68eae03431d23738320609f2a3052ca1ce
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:55:44 +0500

    Transaction

---
+++ src/Kadastr.Platform/Kadastr.Domain/Dtos/Transactions/TransactionCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Kadastr.Domain.Dtos.Transactions
+{
+    public class TransactionCreateDto
+    {
+        public int ParcelID { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
