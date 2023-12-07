fb61a14a00662ff34331f267580482f23c45d02b
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:59:24 +0500

    Get

--- src/School.Platform/School.Service/UseCases/Teachers/Handlers/Get/GetTeaecherImageQueryHandler.cs
+++ src/School.Platform/School.Service/UseCases/Teachers/Handlers/Get/GetTeaecherImageQueryHandler.cs
@@ -1,5 +1,8 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using School.Domain.Dtos.Images;
 using School.Domain.Entities.Teachers;
 using School.Domain.Exceptions.Image;
 using School.Domain.Exceptions.Teacher;
@@ -13,25 +16,47 @@ namespace School.Service.UseCases.Teachers.Handlers.Get
     {
         private readonly IFileService _fileService;
         private readonly IApplicationDbContext _context;
+        private readonly IDistributedCache _distributedCache;
 
-        public GetTeaecherImageQueryHandler(IFileService fileService, IApplicationDbContext context)
+
+        public GetTeaecherImageQueryHandler(IFileService fileService, IApplicationDbContext context, IDistributedCache distributedCache)
         {
             _fileService = fileService;
             _context = context;
+            _distributedCache = distributedCache;
         }
 
         public async Task<byte[]> Handle(GetTeacherImageQuery request, CancellationToken cancellationToken)
         {
-            Teacher? teacher = await _context.Teachers.FirstOrDefaultAsync(x => x.TeacherId == request.TeacherId);
+            string? cache = _distributedCache.GetString("Teacher");
 
-            if (teacher == null)
-                throw new TeacherNotFound();
-            else if (teacher.ImagePath == null)
+            if (cache != null)
+            {
+                List<ImageHelper>? result = JsonConvert.DeserializeObject<List<ImageHelper>>(cache);
+
+                ImageHelper? student = result.FirstOrDefault(x => x.Id == request.TeacherId);
+
+                if (student != null)
+                {
+                    byte[] byteImage = await _fileService.GetImageAsync(student.ImagePath);
+
+                    return byteImage;
+                }
                 throw new ImageNotFound();
+            }
+            else
+            {
+                Teacher? teacher = await _context.Teachers.FirstOrDefaultAsync(x => x.TeacherId == request.TeacherId);
 
-            byte[] image = await _fileService.GetImageAsync(teacher.ImagePath);
+                if (teacher == null)
+                    throw new TeacherNotFound();
+                else if (teacher.ImagePath == null)
+                    throw new ImageNotFound();
 
-            return image;
+                byte[] image = await _fileService.GetImageAsync(teacher.ImagePath);
+
+                return image;
+            }
         }
     }
 }
