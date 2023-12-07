c351721b0bb7ed98a3fe7d1990a1feec2cb45769
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:08:59 +0500

    Auth Service

---
+++ src/Autorization/Autorization.Api/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿using Autorization.Api.Interfaces;
+using Autorization.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Autorization.Api.Services
+{
+    public class AuthService : IAuthService
+    {
+        public string GenerateToken(Login login)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mana-shu-security-key"));
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, login.Email),
+                new Claim(ClaimTypes.Role, login.Role.ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "Issuer",
+                audience: "Audience",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
