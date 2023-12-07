73cece4b5e5cffaecfae9d91720ce73866f8c4b9
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:06:57 +0500

    Get

--- src/School.Platform/School.Service/UseCases/Students/Handlers/Get/GetStudentByIdCommandHandler.cs
+++ src/School.Platform/School.Service/UseCases/Students/Handlers/Get/GetStudentByIdCommandHandler.cs
@@ -1,5 +1,8 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using School.Domain.Dtos.Images;
 using School.Domain.Entities.Students;
 using School.Domain.Exceptions.Students;
 using School.Service.Abstractions.DataAccess;
@@ -10,10 +13,11 @@ namespace School.Service.UseCases.Students.Handlers.Get
     public class GetStudentByIdCommandHandler : IRequestHandler<GetStudentByIdQuery, Student>
     {
         private readonly IApplicationDbContext _context;
-
-        public GetStudentByIdCommandHandler(IApplicationDbContext context)
+        private readonly IDistributedCache _distributedCache;
+        public GetStudentByIdCommandHandler(IApplicationDbContext context, IDistributedCache distributedCache)
         {
             _context = context;
+            _distributedCache = distributedCache;
         }
 
         public async Task<Student> Handle(GetStudentByIdQuery request, CancellationToken cancellationToken)
