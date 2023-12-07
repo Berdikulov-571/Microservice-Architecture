12f20fd70cbb734c49d745528a13ef37fd8e6370
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:57:40 +0500

    Global Exceptions

---
+++ src/Kadastr.Platform/Kadastr.Domain/Exceptions/GlobalException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Kadastr.Domain.Exceptions
+{
+    public class GlobalException : Exception
+    {
+        public HttpStatusCode StatusCode { get; } = HttpStatusCode.NotFound;
+        public string TitleMessage { get; set; } = default!;
+    }
+}
