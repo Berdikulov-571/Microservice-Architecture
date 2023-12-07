42481ff03f842f69508758448fe0277b557bf2da
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:03:59 +0500

    Create

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/LegalDescriptions/Commands/Create/CreateLegalDescriptionCommand.cs
@@ -0,0 +1,10 @@
+﻿using Kadastr.Domain.Dtos.LegalDescriptions;
+using MediatR;
+
+namespace Kadastr.Service.UseCases.LegalDescriptions.Commands.Create
+{
+    public class CreateLegalDescriptionCommand : LegalDescriptionCreateDto, IRequest<int>
+    {
+
+    }
+}
