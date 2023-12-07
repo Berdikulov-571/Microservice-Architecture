a157b3a7151d35a4dd6200dff8e6d6c171ae16f4
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:53:27 +0500

    Migratinons

---
+++ src/Kadastr.Platform/Kadastr.DataAccess/Migrations/20231207060259_Initial.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kadastr.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LandUses",
+                columns: table => new
+                {
+                    LandUseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LandUseType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LandUses", x => x.LandUseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    OwnerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactInfo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.OwnerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parcels",
+                columns: table => new
+                {
+                    ParcelID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerID = table.Column<int>(type: "int", nullable: false),
+                    ParcelNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Area = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parcels", x => x.ParcelID);
+                    table.ForeignKey(
+                        name: "FK_Parcels_Owners_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "Owners",
+                        principalColumn: "OwnerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LegalDescriptions",
+                columns: table => new
+                {
+                    LegalDescriptionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParcelID = table.Column<int>(type: "int", nullable: false),
+                    DescriptionText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LegalDescriptions", x => x.LegalDescriptionID);
+                    table.ForeignKey(
+                        name: "FK_LegalDescriptions_Parcels_ParcelID",
+                        column: x => x.ParcelID,
+                        principalTable: "Parcels",
+                        principalColumn: "ParcelID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PercalLandUses",
+                columns: table => new
+                {
+                    ParcelLandUseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParcelID = table.Column<int>(type: "int", nullable: false),
+                    LandUseID = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PercalLandUses", x => x.ParcelLandUseID);
+                    table.ForeignKey(
+                        name: "FK_PercalLandUses_LandUses_LandUseID",
+                        column: x => x.LandUseID,
+                        principalTable: "LandUses",
+                        principalColumn: "LandUseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PercalLandUses_Parcels_ParcelID",
+                        column: x => x.ParcelID,
+                        principalTable: "Parcels",
+                        principalColumn: "ParcelID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParcelID = table.Column<int>(type: "int", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TransactionType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionID);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Parcels_ParcelID",
+                        column: x => x.ParcelID,
+                        principalTable: "Parcels",
+                        principalColumn: "ParcelID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LegalDescriptions_ParcelID",
+                table: "LegalDescriptions",
+                column: "ParcelID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parcels_OwnerID",
+                table: "Parcels",
+                column: "OwnerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PercalLandUses_LandUseID",
+                table: "PercalLandUses",
+                column: "LandUseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PercalLandUses_ParcelID",
+                table: "PercalLandUses",
+                column: "ParcelID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ParcelID",
+                table: "Transactions",
+                column: "ParcelID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LegalDescriptions");
+
+            migrationBuilder.DropTable(
+                name: "PercalLandUses");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "LandUses");
+
+            migrationBuilder.DropTable(
+                name: "Parcels");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
