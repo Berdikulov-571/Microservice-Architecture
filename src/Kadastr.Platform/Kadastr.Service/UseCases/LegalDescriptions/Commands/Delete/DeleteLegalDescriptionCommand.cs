21352f419698e3357e31ec5a0fcff81eff483ff1
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:04:08 +0500

    Delete Legal

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/LegalDescriptions/Commands/Delete/DeleteLegalDescriptionCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Kadastr.Service.UseCases.LegalDescriptions.Commands.Delete
+{
+    public class DeleteLegalDescriptionCommand : IRequest<int>
+    {
+        public int LegalDescriptionId { get; set; }
+    }
+}
