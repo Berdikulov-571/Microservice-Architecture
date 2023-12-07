3407732094646af60716815ad8d19643d2a99213
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:58:38 +0500

    Get Query Handlers

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/LandUses/Handlers/Get/GetAllLandUseQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Kadastr.Domain.Entities.LandUses;
+using Kadastr.Domain.Exceptions.LandUses;
+using Kadastr.Service.Abstractions.Data;
+using Kadastr.Service.UseCases.LandUses.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kadastr.Service.UseCases.LandUses.Handlers.Get
+{
+    public class GetAllLandUseQueryHandler : IRequestHandler<GetAllLandUseQuery, IEnumerable<LandUse>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllLandUseQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<LandUse>> Handle(GetAllLandUseQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.LandUses.ToListAsync(cancellationToken);
+
+            if (result == null)
+                throw new LandUseNotFound();
+
+            return result;
+        }
+    }
+}
