b0d7f33e1ee6a254549433ce9c81c8ef5c4c0463
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:56:35 +0500

    Percel Land Use

---
+++ src/Kadastr.Platform/Kadastr.Domain/Entities/PercalLandUses/PercalLandUse.cs
@@ -0,0 +1,23 @@
+﻿using Kadastr.Domain.Entities.LandUses;
+using Kadastr.Domain.Entities.Parcels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kadastr.Domain.Entities.PercalLandUses
+{
+    public class PercalLandUse : Auditable
+    {
+        [Key]
+        public int ParcelLandUseID { get; set; }
+
+        [ForeignKey(nameof(Parcel))]
+        public int ParcelID { get; set; }
+        [ForeignKey(nameof(LandUse))]
+        public int LandUseID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public virtual Parcel Parcel { get; set; }
+        public virtual LandUse LandUse { get; set; }
+    }
+}
