3323db63b57f82a4c801805d33ad452daf1dba4d
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:57:10 +0500

    Owners

---
+++ src/Kadastr.Platform/Kadastr.Domain/Exceptions/Owners/OwnerNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace Kadastr.Domain.Exceptions.Owners
+{
+    public class OwnerNotFound : GlobalException
+    {
+        public OwnerNotFound()
+        {
+            TitleMessage = "Owner Not Found !";
+        }
+    }
+}
