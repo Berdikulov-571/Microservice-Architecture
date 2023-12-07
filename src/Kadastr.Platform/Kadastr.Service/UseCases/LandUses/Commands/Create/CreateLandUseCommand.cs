d6ffbe7d7b67c5adf8e6d4298e096fa6095277ca
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:57:56 +0500

    Create

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/LandUses/Commands/Create/CreateLandUseCommand.cs
@@ -0,0 +1,10 @@
+﻿using Kadastr.Domain.Dtos.LandUses;
+using MediatR;
+
+namespace Kadastr.Service.UseCases.LandUses.Commands.Create
+{
+    public class CreateLandUseCommand : LandUseCreateDto, IRequest<int>
+    {
+
+    }
+}
