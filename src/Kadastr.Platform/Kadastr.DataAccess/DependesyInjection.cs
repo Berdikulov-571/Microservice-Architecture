957f1f92ee2ce3044618e2b3371288c9eef51719
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:53:59 +0500

    Data Access

---
+++ src/Kadastr.Platform/Kadastr.DataAccess/DependesyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Kadastr.DataAccess.Persistence.DataContexts;
+using Kadastr.Service.Abstractions.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kadastr.DataAccess
+{
+    public static class DependesyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
