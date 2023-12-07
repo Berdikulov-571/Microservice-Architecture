3034449e7a52007a92aa38cb93f9f9a6d56ad51d
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:01:10 +0500

    Get

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/PercalLandUses/Handlers/Get/GetAllPercelLanUseQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Kadastr.Service.Abstractions.Data;
+using Kadastr.Service.UseCases.PercalLandUses.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kadastr.Service.UseCases.PercalLandUses.Handlers.Get
+{
+    public class GetAllPercelLanUseQueryHandler : IRequestHandler<GetAllPercalLanUseQuery, IEnumerable<Domain.Entities.PercalLandUses.PercalLandUse>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllPercelLanUseQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.PercalLandUses.PercalLandUse>> Handle(GetAllPercalLanUseQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.PercalLandUses.ToListAsync(cancellationToken);
+            
+            return result;
+        }
+    }
+}
