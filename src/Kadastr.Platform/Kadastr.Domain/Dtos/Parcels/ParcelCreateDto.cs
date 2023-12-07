5d50a88387dcaede837d18e3aeb3fd4926346f18
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:54:47 +0500

    Percels

---
+++ src/Kadastr.Platform/Kadastr.Domain/Dtos/Parcels/ParcelCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Kadastr.Domain.Dtos.Parcels
+{
+    public class ParcelCreateDto
+    {
+        public int OwnerID { get; set; }
+        public string ParcelNumber { get; set; }
+        public decimal Area { get; set; }
+    }
+}
---
+++ src/Kadastr.Platform/Kadastr.Domain/Dtos/Parcels/ParcelUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Kadastr.Domain.Dtos.Parcels
+{
+    public class ParcelUpdateDto
+    {
+        public int ParcelID { get; set; }
+        public int OwnerID { get; set; }
+        public string ParcelNumber { get; set; }
+        public decimal Area { get; set; }
+    }
+}
