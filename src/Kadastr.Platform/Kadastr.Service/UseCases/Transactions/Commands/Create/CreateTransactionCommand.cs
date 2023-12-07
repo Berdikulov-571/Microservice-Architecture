3b68c8fb060f958b6fc0b0f5df294d84178c6fb5
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:00:58 +0500

    Transaction

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/Transactions/Commands/Create/CreateTransactionCommand.cs
@@ -0,0 +1,9 @@
+﻿using Kadastr.Domain.Dtos.Transactions;
+using MediatR;
+
+namespace Kadastr.Service.UseCases.Transactions.Commands.Create
+{
+    public class CreateTransactionCommand : TransactionCreateDto, IRequest<int>
+    {
+    }
+}
