03b211076973a4255cad642e3ac691ae6c8ddaf3
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:08:34 +0500

    Appliccation Db Context

---
+++ src/Autorization/Autorization.DataAccess/DataContexts/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Autorization.DataAccess.DataContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
