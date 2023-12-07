50f4b4b2382f5feccbafafe28b51c779142a4dda
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 11:56:21 +0500

    Legal Description

---
+++ src/Kadastr.Platform/Kadastr.Domain/Entities/LegalDescriptions/LegalDescription.cs
@@ -0,0 +1,16 @@
+﻿using Kadastr.Domain.Entities.Parcels;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kadastr.Domain.Entities.LegalDescriptions
+{
+    public class LegalDescription : Auditable
+    {
+        public int LegalDescriptionID { get; set; }
+        [ForeignKey(nameof(Parcel))]
+        public int ParcelID { get; set; }
+        public string DescriptionText { get; set; }
+
+        public virtual Parcel Parcel { get; set; }
+    }
+
+}
