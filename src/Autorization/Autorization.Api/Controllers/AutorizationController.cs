63d7c9b1a8228c95fbfd70f269dd845f5f3073bf
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:07:38 +0500

    Auth Controller

---
+++ src/Autorization/Autorization.Api/Controllers/AutorizationController.cs
@@ -0,0 +1,26 @@
+﻿using Autorization.Api.Interfaces;
+using Autorization.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autorization.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AutorizationController : ControllerBase
+    {
+        private readonly IAuthService _tokenGenerator;
+
+        public AutorizationController(IAuthService tokenGenerator)
+        {
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> Login(Login login)
+        {
+            string token = _tokenGenerator.GenerateToken(login);
+
+            return Ok(token);
+        }
+    }
+}
