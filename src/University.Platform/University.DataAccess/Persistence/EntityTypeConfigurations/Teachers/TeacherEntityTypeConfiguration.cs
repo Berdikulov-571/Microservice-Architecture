b729b3dd4f6d6bc6436ee0032f67fe35128ce3d3
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 15:14:11 +0500

    Configuration

---
+++ src/University.Platform/University.DataAccess/Persistence/EntityTypeConfigurations/Teachers/TeacherEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+using University.Domain.Entities.Teachers;
+
+namespace University.DataAccess.Persistence.EntityTypeConfigurations.Teachers
+{
+    public class TeacherEntityTypeConfiguration : IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.HasKey(t => t.TeacherId);
+
+            builder.HasMany(t => t.Courses)
+                .WithOne(c => c.Teacher)
+                .HasForeignKey(c => c.TeacherId);
+        }
+    }
+}
