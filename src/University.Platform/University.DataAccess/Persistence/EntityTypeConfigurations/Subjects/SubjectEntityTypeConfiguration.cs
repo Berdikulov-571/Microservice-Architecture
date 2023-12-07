82eecd06f718ac5ec509670598988e5b59b05bea
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 15:13:49 +0500

    Configuration

---
+++ src/University.Platform/University.DataAccess/Persistence/EntityTypeConfigurations/Subjects/SubjectEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+using University.Domain.Entities.Subjects;
+
+namespace University.DataAccess.Persistence.EntityTypeConfigurations.Subjects
+{
+    public class SubjectEntityTypeConfiguration : IEntityTypeConfiguration<Subject>
+    {
+        public void Configure(EntityTypeBuilder<Subject> builder)
+        {
+            builder.HasKey(s => s.SubjectId);
+        }
+    }
+}
