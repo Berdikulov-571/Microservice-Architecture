3a5a78a49ce226f9e4d0fcd077f4dbbb51bc4ff5
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:02:35 +0500

    Get

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/Parcels/Handlers/Get/GetParcelByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Kadastr.Domain.Entities.Parcels;
+using Kadastr.Domain.Exceptions.Parcels;
+using Kadastr.Service.Abstractions.Data;
+using Kadastr.Service.UseCases.Parcels.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kadastr.Service.UseCases.Parcels.Handlers.Get
+{
+    public class GetParcelByIdQueryHandler : IRequestHandler<GetParcelByIdQuery, Parcel>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetParcelByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Parcel> Handle(GetParcelByIdQuery request, CancellationToken cancellationToken)
+        {
+            var parcel = await _context.Parcels.FirstOrDefaultAsync(x => x.ParcelID == request.ParcelId, cancellationToken);
+
+            if (parcel == null)
+                throw new ParcelNotFound();
+            
+            return parcel;
+        }
+    }
+}
