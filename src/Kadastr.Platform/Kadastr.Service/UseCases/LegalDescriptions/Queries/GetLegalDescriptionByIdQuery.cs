a53f117a4bd44aaf6f333d71139f6e9805dceb6e
berdikulov-571 <mygithub571@gmail.com>
2023-12-07 12:05:12 +0500

    Queroes

---
+++ src/Kadastr.Platform/Kadastr.Service/UseCases/LegalDescriptions/Queries/GetLegalDescriptionByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Kadastr.Domain.Entities.LegalDescriptions;
+using MediatR;
+
+namespace Kadastr.Service.UseCases.LegalDescriptions.Queries
+{
+    public class GetLegalDescriptionByIdQuery : IRequest<LegalDescription>
+    {
+        public int LegalDescriptionId { get; set; } 
+    }
+}
