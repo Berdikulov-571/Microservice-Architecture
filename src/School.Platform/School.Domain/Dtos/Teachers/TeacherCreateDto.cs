90207eb014322104be8c69c3e0075058a1568e6b
berdikulov-571 <mygithub571@gmail.com>
2023-12-08 08:00:50 +0500

    Teacher Create Dto

--- src/School.Platform/School.Domain/Dtos/Teachers/TeacherCreateDto.cs
+++ src/School.Platform/School.Domain/Dtos/Teachers/TeacherCreateDto.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using School.Domain.Attributes.Email;
+using School.Domain.Attributes.Length;
+using School.Domain.Attributes.Password;
+using School.Domain.Attributes.PhoneNumber;
 using School.Domain.Enums.GenderEnum;
 using School.Domain.Enums.RoleEnum;
 
@@ -6,14 +10,21 @@ namespace School.Domain.Dtos.Teachers
 {
     public class TeacherCreateDto
     {
+        [Length]
         public string UserName { get; set; }
+        [Length]
         public string FirstName { get; set; }
+        [Length]
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
+        [PhoneNumber]
         public string PhoneNumber { get; set; }
+        [Length]
         public string Address { get; set; }
+        [Password]
         public string PasswordHash { get; set; }
+        [Email]
         public string Email { get; set; }
         public IFormFile ImagePath { get; set; }
     }
