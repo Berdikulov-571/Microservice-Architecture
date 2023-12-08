1b1baf7f68f58770eb87c04ecde3710381008edb
berdikulov-571 <mygithub571@gmail.com>
2023-12-08 12:11:06 +0500

    User Not Found

---
+++ src/Autorization/Autorization.Api/Exceptions/UserNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Autorization.Api.Exceptions
+{
+    public class UserNotFound : Exception
+    {
+        public string Title { get; set; }
+        public UserNotFound()
+        {
+            Title = "User Not Found !";
+        }
+    }
+}
