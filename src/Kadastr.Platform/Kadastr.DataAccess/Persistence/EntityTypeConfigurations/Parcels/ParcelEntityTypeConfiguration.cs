eaa3caef73048fa9f9d1d18391d5d37ee86dc221
berdikulov-571 <mygithub571@gmail.com>
2023-12-08 12:15:08 +0500

    Parcel Entity Type Configuration

---
+++ src/Kadastr.Platform/Kadastr.DataAccess/Persistence/EntityTypeConfigurations/Parcels/ParcelEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Kadastr.Domain.Entities.Parcels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kadastr.DataAccess.Persistence.EntityTypeConfigurations.Parcels
+{
+    public class ParcelEntityTypeConfiguration : IEntityTypeConfiguration<Parcel>
+    {
+        public void Configure(EntityTypeBuilder<Parcel> builder)
+        {
+            builder.HasOne(p => p.Owner)
+            .WithMany(o => o.Parcels)
+            .HasForeignKey(p => p.OwnerID);
+        }
+    }
+}
