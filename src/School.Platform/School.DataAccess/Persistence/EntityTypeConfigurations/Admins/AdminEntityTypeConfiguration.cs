83503b8d7570e5e2a956e669af4b0ee80c91f817
berdikulov-571 <mygithub571@gmail.com>
2023-12-08 08:01:05 +0500

    Admin Entity Type Configurations

--- src/School.Platform/School.DataAccess/Persistence/EntityTypeConfigurations/Admins/AdminEntityTypeConfiguration.cs
+++ src/School.Platform/School.DataAccess/Persistence/EntityTypeConfigurations/Admins/AdminEntityTypeConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using School.Domain.Entities.Admins;
+using School.Service.Security.Password;
 
 namespace School.DataAccess.Persistence.EntityTypeConfigurations.Admins
 {
@@ -9,6 +10,18 @@ namespace School.DataAccess.Persistence.EntityTypeConfigurations.Admins
         public void Configure(EntityTypeBuilder<Admin> builder)
         {
             builder.HasKey(x => x.AdminId);
+
+            builder.HasData(new Admin()
+            {
+                AdminId = 1,
+                Email = "bsanjarbek06@gmail.com",
+                FirstName = "Sanjarbek",
+                LastName = "Berdikulov",
+                PasswordHash = Hash512.ComputeSHA512HashFromString("sanjarbek2006"),
+                Role = Domain.Enums.RoleEnum.Role.SuperAdmin,
+                UserName = "Berdikulov_571",
+                CreatedAt = DateTime.Now,
+            });
         }
     }
 }
