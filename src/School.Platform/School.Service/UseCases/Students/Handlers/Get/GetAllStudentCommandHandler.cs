633132a841112251c22515835e6a0ab752292da3
berdikulov-571 <mygithub571@gmail.com>
2023-12-08 12:12:18 +0500

    Get All Students Command

--- src/School.Platform/School.Service/UseCases/Students/Handlers/Get/GetAllStudentCommandHandler.cs
+++ src/School.Platform/School.Service/UseCases/Students/Handlers/Get/GetAllStudentCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using School.Domain.Entities.Students;
 using School.Domain.Exceptions.Students;
 using School.Service.Abstractions.DataAccess;
@@ -10,19 +12,31 @@ namespace School.Service.UseCases.Students.Handlers.Get
     public class GetAllStudentCommandHandler : IRequestHandler<GetAllStudentQuery, IEnumerable<Student>>
     {
         private readonly IApplicationDbContext _context;
+        private readonly IDistributedCache _cache;
 
-        public GetAllStudentCommandHandler(IApplicationDbContext context)
+        public GetAllStudentCommandHandler(IApplicationDbContext context, IDistributedCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         public async Task<IEnumerable<Student>> Handle(GetAllStudentQuery request, CancellationToken cancellationToken)
         {
+
+            string? response = _cache.GetString("GetAllStudents");
+
+            if (response != null)
+            {
+                var cacheStudents = JsonConvert.DeserializeObject<List<Student>>(response);
+
+                return cacheStudents;
+            }
+
             IEnumerable<Student> students = await _context.Students.ToListAsync(cancellationToken);
 
             if (students == null)
                 throw new StudentNotFound();
-
+            
             return students;
         }
     }
