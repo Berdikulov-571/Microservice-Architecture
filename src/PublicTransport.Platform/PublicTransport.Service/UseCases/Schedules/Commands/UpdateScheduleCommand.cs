5c53ec3d5cba39d309d112dab36134679800dceb
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:07:04 +0500

    Commands

---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Schedules/Commands/UpdateScheduleCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PublicTransport.Domain.Dtos.Schedules;
+
+namespace PublicTransport.Service.UseCases.Schedules.Commands
+{
+    public class UpdateScheduleCommand : ScheduleUpdateDto, IRequest<int>
+    {
+
+    }
+}
