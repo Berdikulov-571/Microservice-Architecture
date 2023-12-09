bd80b45ef7908bfaf2168ea72546ca660e6fa623
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:05:43 +0500

    Roles

---
+++ src/PublicTransport.Platform/PublicTransport.Domain/Enums/Roles/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace PublicTransport.Domain.Enums.Roles
+{
+    public enum Role
+    {
+        SuperAdmin = 1,
+        Admin,
+        Developer,
+        Teacher,
+        Student,
+        User,
+    }
+}
