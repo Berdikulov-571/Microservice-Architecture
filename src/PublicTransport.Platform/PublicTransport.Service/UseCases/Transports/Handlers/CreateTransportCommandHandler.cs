65810c318360770c9853196b7a547339f428b032
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:09:31 +0500

    Handlers

---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Transports/Handlers/CreateTransportCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using PublicTransport.Domain.Entities.Transports;
+using PublicTransport.Service.Abstractions.DataContexts;
+using PublicTransport.Service.UseCases.Transports.Commands;
+
+namespace PublicTransport.Service.UseCases.Transports.Handlers
+{
+    public class CreateTransportCommandHandler : IRequestHandler<CreateTransportCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        
+        public CreateTransportCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateTransportCommand request, CancellationToken cancellationToken)
+        {
+            Transport transport = new Transport()
+            {
+                Capacity = request.Capacity,
+                TransportName = request.TransportName,
+                TransportType = request.TransportType,
+            };
+
+            await _context.Transports.AddAsync(transport,cancellationToken);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Transports/Handlers/DeleteTransportCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PublicTransport.Service.Abstractions.DataContexts;
+using PublicTransport.Service.UseCases.Transports.Commands;
+
+namespace PublicTransport.Service.UseCases.Transports.Handlers
+{
+    public class DeleteTransportCommandHandler : IRequestHandler<DeleteTransportCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteTransportCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteTransportCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Transports.FirstOrDefaultAsync(x => x.Id == request.TransportId, cancellationToken);
+
+            if (result == null)
+            {
+                return 0;
+            }
+
+            _context.Transports.Remove(result);
+            int res = await _context.SaveChangesAsync(cancellationToken);
+
+            return res;
+        }
+    }
+}
