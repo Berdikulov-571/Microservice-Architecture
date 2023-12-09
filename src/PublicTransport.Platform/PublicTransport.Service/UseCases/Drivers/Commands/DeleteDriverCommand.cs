c8966848fba0fef19438a714e28d1c21bcdcf2df
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:05:58 +0500

    Commands

---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Drivers/Commands/DeleteDriverCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PublicTransport.Service.UseCases.Drivers.Commands
+{
+    public class DeleteDriverCommand : IRequest<int>
+    {
+        public int DriverId { get; set; }
+    }
+}
