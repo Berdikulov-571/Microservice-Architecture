a13bf0c3e75c5430f4c30c904e68f200a4cfc386
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:09:13 +0500

    Queries

---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Transports/Queries/GetTransportByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PublicTransport.Domain.Entities.Transports;
+
+namespace PublicTransport.Service.UseCases.Transports.Queries
+{
+    public class GetTransportByIdQuery : IRequest<Transport>
+    {
+        public int TransportId { get; set; }
+    }
+}
