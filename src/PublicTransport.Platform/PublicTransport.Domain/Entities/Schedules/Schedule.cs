8585290dad1f2f95bd5fae7574b5f3de1f9cfc31
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:05:30 +0500

    Schedules

---
+++ src/PublicTransport.Platform/PublicTransport.Domain/Entities/Schedules/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using PublicTransport.Domain.Entities.Routes;
+using PublicTransport.Domain.Entities.Transports;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PublicTransport.Domain.Entities.Schedules
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int TransportId { get; set; }
+
+        [ForeignKey(nameof(Route))]
+        public int RouteId { get; set; }
+
+        public Route Route { get; set; }
+        public Transport Transport { get; set; }
+    }
+}
