b6ff7c996f590751d4b088c07400385a7599c810
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:06:58 +0500

    Handlers

---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Schedules/Handlers/GetAllScheduleQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PublicTransport.Domain.Entities.Schedules;
+using PublicTransport.Service.Abstractions.DataContexts;
+using PublicTransport.Service.UseCases.Schedules.Queries;
+
+namespace PublicTransport.Service.UseCases.Schedules.Handlers
+{
+    public class GetAllScheduleQueryHandler : IRequestHandler<GetAllScheduleQuery, IEnumerable<Schedule>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllScheduleQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Schedule>> Handle(GetAllScheduleQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Schedules.ToListAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Schedules/Handlers/UpdateScheduleCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PublicTransport.Domain.Entities.Schedules;
+using PublicTransport.Service.Abstractions.DataContexts;
+using PublicTransport.Service.UseCases.Schedules.Commands;
+
+namespace PublicTransport.Service.UseCases.Schedules.Handlers
+{
+    public class UpdateScheduleCommandHandler : IRequestHandler<UpdateScheduleCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateScheduleCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateScheduleCommand request, CancellationToken cancellationToken)
+        {
+            Schedule schedule = await _context.Schedules.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (schedule == null)
+            {
+                return 0;
+            }
+            schedule.RouteId = request.RouteId;
+            schedule.TransportId = request.TransportId;
+            schedule.DepartureTime = DateTime.Now;
+
+            _context.Schedules.Remove(schedule);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
