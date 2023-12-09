897a64fbbe75dda17de9b80bde7102fea342cfd4
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:04:19 +0500

    Drivers

---
+++ src/PublicTransport.Platform/PublicTransport.Domain/Dtos/Drivers/DriverCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PublicTransport.Domain.Dtos.Drivers
+{
+    public class DriverCreateDto
+    {
+        public string DriveName { get; set; }
+        public int LicenseNumber { get; set; }
+
+        public int TransportId { get; set; }
+    }
+}
