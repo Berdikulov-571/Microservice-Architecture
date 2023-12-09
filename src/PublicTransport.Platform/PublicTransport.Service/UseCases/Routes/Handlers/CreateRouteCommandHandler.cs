66dbc73231c32e3b0fbc8aa447436010f20da7ce
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:06:34 +0500

    Handlers

---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Routes/Handlers/CreateRouteCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using PublicTransport.Domain.Entities.Routes;
+using PublicTransport.Service.Abstractions.DataContexts;
+using PublicTransport.Service.UseCases.Routes.Commands;
+
+namespace PublicTransport.Service.UseCases.Routes.Handlers
+{
+    public class CreateRouteCommandHandler : IRequestHandler<CreateRouteCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateRouteCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateRouteCommand request, CancellationToken cancellationToken)
+        {
+            Route route = new Route()
+            {
+                RouteName = request.RouteName,
+                EndLocation = request.EndLocation,
+                StartLocation = request.StartLocation,
+            };
+
+            await _context.Routes.AddAsync(route);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
---
+++ src/PublicTransport.Platform/PublicTransport.Service/UseCases/Routes/Handlers/UpdateRouteCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PublicTransport.Domain.Entities.Routes;
+using PublicTransport.Service.Abstractions.DataContexts;
+using PublicTransport.Service.UseCases.Routes.Commands;
+
+namespace PublicTransport.Service.UseCases.Routes.Handlers
+{
+    public class UpdateRouteCommandHandler : IRequestHandler<UpdateRouteCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateRouteCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateRouteCommand request, CancellationToken cancellationToken)
+        {
+            Route route = await _context.Routes.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            route.StartLocation = request.StartLocation;
+            route.EndLocation = request.EndLocation;
+            route.RouteName = request.RouteName;
+
+            _context.Routes.Update(route);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
