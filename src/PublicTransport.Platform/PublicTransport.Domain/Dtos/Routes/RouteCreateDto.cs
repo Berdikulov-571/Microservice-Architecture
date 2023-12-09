acce4e33f087ea0bb998f89a496cc6d575a862d0
berdikulov-571 <mygithub571@gmail.com>
2023-12-09 13:04:37 +0500

    Route

---
+++ src/PublicTransport.Platform/PublicTransport.Domain/Dtos/Routes/RouteCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PublicTransport.Domain.Dtos.Routes
+{
+    public class RouteCreateDto
+    {
+        public string RouteName { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+    }
+}
