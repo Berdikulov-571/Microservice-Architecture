ecf0284f30a56c1807f5c811f01fff1b64dc5eef
berdikulov-571 <mygithub571@gmail.com>
2023-12-13 14:58:47 +0500

    SubjectAlreadyExists

---
+++ src/University.Platform/University.Domain/Exceptions/Subjects/SubjectAlreadyExists.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.Subjects
+{
+    public class SubjectAlreadyExists : GlobalException
+    {
+        public SubjectAlreadyExists()
+        {
+            TitleMessage = "Subject Already Exists !";
+        }
+    }
+}
