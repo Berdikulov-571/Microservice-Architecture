f1c56e7bb23632e896f7f016a6763121cc148b55
berdikulov-571 <mygithub571@gmail.com>
2023-12-13 14:59:28 +0500

    CreateSubjectCommandTest

---
+++ UnitTesting/University.Platform.UnitTesting/Commands/CreateSubjectCommandTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using University.Service.Abstractions.DataContexts;
+
+namespace University.Platform.UnitTesting.Commands
+{
+    public class CreateSubjectCommandTest : IClassFixture<DependecyInjection>
+    {
+        private readonly IApplicationDbContext _context;
+        public CreateSubjectCommandTest(DependecyInjection dependecyInjection)
+        {
+            _context = dependecyInjection.ServiceProvider.GetRequiredService<IApplicationDbContext>();
+        }
+
+
+        //[Fact]
+        //public async Task Create_Subject_Command()
+        //{
+            //// Arrange
+            //var cancellationToken = new CancellationTokenSource().Token;
+            //CreateSubjectCommand command = new CreateSubjectCommand()
+            //{
+            //    Name = name
+            //};
+
+            //  CreateSubjectCommandHandler handler = new CreateSubjectCommandHandler(_context);
+
+            //// Act
+            //Action result = async () => await handler.Handle(command, cancellationToken);
+
+            //// Assert
+            //Assert.Throws<SubjectAlreadyExists>(result);
+        //}
+    }
+}
