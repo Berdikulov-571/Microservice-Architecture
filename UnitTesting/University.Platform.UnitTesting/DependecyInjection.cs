f7377b762d92a2d31042b58a3e6b8466e64eadb5
berdikulov-571 <mygithub571@gmail.com>
2023-12-13 14:59:43 +0500

    DependecyInjection

---
+++ UnitTesting/University.Platform.UnitTesting/DependecyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Autorization.Api.DataContexts;
+using Autorization.Api.Interfaces;
+using Autorization.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace University.Platform.UnitTesting
+{
+    public class DependecyInjection
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public DependecyInjection()
+        {
+            IServiceCollection service = new ServiceCollection();
+
+            //service.AddDbContext<IApplicationDbContext, FakeDbContext>(options =>
+            //{
+            //    options.UseInMemoryDatabase("FakeDbContext");
+            //});
+
+            service.AddScoped<IAuthService, AuthService>();
+
+            service.AddDbContext<SchoolDatabase>(options => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;DataBase=School.Platform;Trusted_Connection=True;TrustServerCertificate=True;"));
+            service.AddDbContext<UniversityDatabase>(options => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;DataBase=University.Platform;Trusted_Connection=True;TrustServerCertificate=True;"));
+
+
+            ServiceProvider = service.BuildServiceProvider();
+        }
+    }
+}
