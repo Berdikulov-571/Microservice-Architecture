9ef0ad196697c2e4d367d49f48ed48016ac6125f
berdikulov-571 <mygithub571@gmail.com>
2023-12-14 11:24:00 +0500

    DependecyInjection

---
+++ UnitTesting/Transport.Platform.UnitTesting/DependecyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PublicTransport.Service.Abstractions.DataContexts;
+
+namespace Transport.Platform.UnitTesting
+{
+    public class DependecyInjection
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public DependecyInjection()
+        {
+            IServiceCollection service = new ServiceCollection();
+
+            service.AddDbContext<IApplicationDbContext, FakeDbContext>(options => options.UseInMemoryDatabase("FakeDbContext"));
+
+            ServiceProvider = service.BuildServiceProvider();
+        }
+    }
+}
